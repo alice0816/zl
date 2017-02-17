d66709eb87f3eea8027d9e1ba07c4f43c013f9e1
wufeng.jiang <jwf1990@126.com>
2017-02-17 08:58:16 +0800

    sort component

--- BomTool/bomtool.py
+++ BomTool/bomtool.py
@@ -10,7 +10,7 @@ finally:
 columnline = lines[1]
 columnNames = columnline.split('\t')
 totalColumn = len(columnNames)
-print
+
 
 def getNeedColumnNo():
     needColumnNolist = []
@@ -47,9 +47,9 @@ def saveNeedColumnValues():
     
 def mergeRows():
     
-    mergeCsvfile = file(r'D:\Alice\Project\Python\AnalysisData\x1000.csv', 'wb')  # output file
+    mergeCsvfile = file(r'D:\Alice\Project\Python\AnalysisData\x1000.csv', 'wb')  
     writer = csv.writer(mergeCsvfile)
-    writer.writerow(['Item', 'Quantity', 'Reference', 'Part', 'PCB Footprint'])
+    writer.writerow(['Quantity', 'Reference', 'Part', 'PCB Footprint'])
     
     csvfile = file(r'D:\Alice\Project\Python\AnalysisData\tmp.csv', 'r+')
     reader = csv.reader(csvfile)
@@ -60,7 +60,7 @@ def mergeRows():
     for i in range(rowsLen):
 
         if rows[(rowsLen-i-1 )][0].lower().startswith('t'):
-            if not ((rows[(rowsLen-i-1 )][1].lower().startswith('test') or rows[(rowsLen-i-1)][1].lower().endswith('point'))):
+            if not (('test' in rows[(rowsLen-i-1 )][1].lower()) or('point' in rows[(rowsLen-i-1)][1].lower())):
                 print rows[(rowsLen-i-1 )][0]
                 raise Exception('Reference with T,but not the "Test poist"!!!!')
             del rows[(rowsLen-i-1 )]  
@@ -83,7 +83,7 @@ def mergeRows():
             i += 1
             
         item += 1
-        writer.writerow([(item), (countQuantity), (referenceValue), (row[1]), (row[2])])
+        writer.writerow([(countQuantity), (referenceValue), (row[1]), (row[2])])
         
         _leng = len(needDeleteRows)
         for i in range(_leng):
@@ -91,6 +91,43 @@ def mergeRows():
             
     mergeCsvfile.close()
     csvfile.close()
+    
+def sortComponents():
+    sortfile = file(r'D:\Alice\Project\Python\AnalysisData\sort.csv', 'wb')  # output file
+    writer = csv.writer(sortfile)
+    writer.writerow(['Quantity', 'Reference', 'Part', 'PCB Footprint'])
+    
+    csvfile = file(r'D:\Alice\Project\Python\AnalysisData\x1000.csv', 'r+')
+    reader = csv.reader(csvfile)
+    rows =  [row for row in reader][1:]
+    
+    orderBasis = ['U', 'C', 'R', 'D', 'A', 'B', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+                'N', 'O', 'P', 'Q', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+    for order in orderBasis:
+        firstOrderResult = []
+        for row in rows:
+            if row[1].upper().startswith(order):
+                firstOrderResult.append(row)
+                
+        for i in range(len(firstOrderResult)):
+            for j in range(i):
+                a = firstOrderResult[j][1][1]
+                b = firstOrderResult[j+1][1][1]
+                if (a.upper() in orderBasis):
+                    a = ord(a.upper())
+                else:
+                    a = int(a)
+                if (b.upper() in orderBasis):
+                    b = ord(b.upper())
+                else:
+                    b = int(b)
+                    
+                if a > b:
+                    firstOrderResult[j], firstOrderResult[j + 1] = firstOrderResult[j + 1], firstOrderResult[j]
         
+        for result in firstOrderResult:
+            writer.writerow(result)
+                  
 saveNeedColumnValues()    
-mergeRows()   
+mergeRows()  
+sortComponents() 
